f94c1414fa946f6ab7b8d7fc85fba71de6e045cf
devferx <ferkki@hotmail.com>
2021-09-24 22:18:29 -0400

    :construction: [UPDATE] Refactor state in usePomodoro hook

--- src/hooks/usePomodoro.js
+++ src/hooks/usePomodoro.js
@@ -1,14 +1,14 @@
 import { useState, useRef, useEffect } from 'react'
 
-const POMODORO_DURATION = 25 * 60
-const SHORT_BREAK_DURATION = 5 * 60
+// const POMODORO_DURATION = 25 * 60
+// const SHORT_BREAK_DURATION = 5 * 60
+const POMODORO_DURATION = 5
+const SHORT_BREAK_DURATION = 3
 
 export const usePomodoro = () => {
-  const [state, setState] = useState({
-    secondsElapsed: 0,
-    time: POMODORO_DURATION,
-    isBreak: false,
-  })
+  const [time, setTime] = useState(POMODORO_DURATION)
+  const [isBreak, setIsBreak] = useState(false)
+  const [secondsElapsed, setSecondsElapsed] = useState(0)
   const [isRunning, setIsRunning] = useState(false)
   const [pauseText, setPauseText] = useState('Pause')
 
@@ -20,26 +20,20 @@ export const usePomodoro = () => {
     timer.current = setInterval(() => {
       if (isPaused.current) return
 
-      setState((prevState) => {
-        if (prevState.secondsElapsed === prevState.time) {
-          window.clearInterval(timer.current)
-          setIsRunning(false)
-
-          return {
-            ...prevState,
-            time: prevState.isBreak ? POMODORO_DURATION : SHORT_BREAK_DURATION,
-            secondsElapsed: 0,
-            isBreak: !prevState.isBreak,
-          }
-        }
-
-        return {
-          ...prevState,
-          secondsElapsed: prevState.secondsElapsed + 1,
-        }
-      })
+      setSecondsElapsed((prevState) => prevState + 1)
     }, 1000)
   }
+
+  useEffect(() => {
+    if (secondsElapsed === time) {
+      window.clearInterval(timer.current)
+      setIsRunning(false)
+
+      setSecondsElapsed(0)
+      setIsBreak(!isBreak)
+      setTime(isBreak ? POMODORO_DURATION : SHORT_BREAK_DURATION)
+    }
+  }, [secondsElapsed])
 
   const pausePomodoro = () => {
     isPaused.current = !isPaused.current
@@ -47,7 +41,9 @@ export const usePomodoro = () => {
   }
 
   return {
-    ...state,
+    secondsElapsed,
+    isBreak,
+    time,
     isRunning,
     isPaused: isPaused.current,
     pauseText,
